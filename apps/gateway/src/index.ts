b929a9d64c34fc949758f23938825b9483ad3359
LovelySehotraPsquare <lovely.sehotra@psquarecompany.com>
2026-04-17 16:28:46 +0530

    setup::gateway app

---
+++ apps/gateway/src/index.ts
@@ -0,0 +1,30 @@
+
+import { createApp } from "./server";
+
+const start = async () => {
+  try {
+    // await runMigrations();
+    console.log('✓ Migrations applied');
+
+    const app = await createApp();
+
+    const address = await app.listen({ port: 3002 ,host: '0.0.0.0' });
+    console.log(`🚀 Gateway listening on ${address}`);
+
+    const gracefulShutdown = async (signal: string) => {
+      console.log(`\n${signal} received, shutting down gracefully...`);
+      await app.close();
+    //   await closeConnection();
+      console.log('✓ Shutdown complete');
+      process.exit(0);
+    };
+
+    process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+    process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+  } catch (error) {
+    console.error('Failed to start:', error);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ apps/gateway/src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify';
+import helmet from '@fastify/helmet';
+import cors from '@fastify/cors';
+// import rateLimit from '@fastify/rate-limit';
+// import { env } from '@relay/config';
+
+export async function createApp() {
+  const fastify = Fastify({
+    // logger: {
+    //   level: env.LOG_LEVEL,
+    //   transport: env.NODE_ENV === 'development' ? { target: 'pino-pretty' } : undefined,
+    // },
+  });
+
+  await fastify.register(helmet);
+  await fastify.register(cors, { origin: true });
+  // await fastify.register(rateLimit, {
+  //   redis: fastify.redis,
+  //   global: false,
+  // });
+  fastify.addHook('onRequest', async (request, reply) => {
+    request.log.info({ method: request.method, url: request.url }, 'incoming request');
+  });
+
+  return fastify;
+}
